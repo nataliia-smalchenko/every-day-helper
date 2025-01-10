e2734d0c6fd3b4d8639d4e21c138044da7dd99e1
Maryna-Korbet <m22v86k@gmail.com>
2025-01-10 10:28:12 -0500

    change animation

--- src/animations.py
+++ src/animations.py
@@ -3,15 +3,13 @@ import time
 import sys
 
 def running_text_animation():
-    greeting = "     Welcome to the assistant bot!     "
+    greeting = "Welcome to the assistant bot! "
 
     for i in range(len(greeting) * 1):
-        display_text = (greeting * 1)[i:i+len(greeting)]
-        
+        display_text = greeting[:i]
+        sys.stdout.write(f'\r')
+        sys.stdout.flush()
         print_formatted_text(HTML(f'<cyan>{display_text}</cyan>'), end='', flush=True)
-        
-        time.sleep(0.05)
-        
-        sys.stdout.write('\r')
+        time.sleep(0.06)
         
     print()
